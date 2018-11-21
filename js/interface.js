63c8b9b8594f7dc7ce82992239768917d0d0339b
Carlos Peña <corochena@gmail.com>
2018-11-20 18:47:16 -0600

    basic interface done

--- js/interface.js
+++ js/interface.js
@@ -3,7 +3,8 @@ var order=2, n=5;
 crearTabla(n,order+1);
 
 function crearTabla(rows, cols) {
-  // modificar esta funcion para que aparezcan encabezados X1, X2, X3...B
+  // modificar esta funcion para que aparezcan encabezados  de columna X1, X2, X3...Y
+  // y de fila 1, 2, 3, ... n
   var esp = document.querySelector(".datainput");
   if (esp.firstChild) esp.removeChild(esp.firstChild);
 
@@ -25,36 +26,28 @@ function crearTabla(rows, cols) {
 
 document.querySelector("#order").addEventListener("input", function(e) {
   order = +e.target.value;
-  crearTabla(order+1,n);
+  crearTabla(n,order+1);
 });
 
 document.querySelector("#n").addEventListener("input", function(e) {
   n = +e.target.value;
-  crearTabla(order+1,n);
+  crearTabla(n,order+1);
 });
 
-// console.log(gauss([[1, 1, 1], [1, -1, 1], [-2, 3, 1]], [16, 10, 7]));
-// devuelve 5,3,8
-
 document.querySelector("button").addEventListener("click", function() {
-  /* leer los valores de los input de la tabla
-     y los vamos a introducir en una SOLA matriz de 
-     n filas y n + 1 columnas */
-  /* llamar a la funcion gauss usando esa unica matriz */
-  var Aflat = [],
-    A = [],
-    B = [];
+  var data = [];
+  
   var elementos = document.querySelectorAll("table input");
 
   for (var i = 0; i < elementos.length; i++) {
     var v = +elementos[i].value;
-    if ((i + 1) % (n + 1) == 0) B.push(v);
-    else Aflat.push(v);
+    if (i <= order) {
+      data.push([]);
+      data[i].push(v);
+    }
+    else {
+      data[i%(order+1)].push(v);
+    }
   }
-
-  for (var i = 0; i < n; i++) A.push(Aflat.slice(n * i, n * (i + 1)));
-
-  // en lugar de escribir texto escriban html y den algun formato
-  // cambiar el mensaje a X1 = ..., X2 = ..., X3 ... Xn = ...
-  document.querySelector("#respuesta").textContent = gauss(A, B);
+  var reg = regresion(data);
 });
